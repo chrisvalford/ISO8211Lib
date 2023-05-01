3ac0d9e5618d7e1951e0678b44a9d694174934cf
Christopher Alford <chrisvalford@me.com>
2023-05-01 19:26:30 +0200

    Initial Commit

---
+++ ISO8211Lib/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ISO8211Lib
+//
+//  Created by Christopher Alford on 1/5/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
